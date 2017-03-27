37ab2ae15fcd85d0102257f0db6dd314f3ff3e15
YuTong-Zhang <juliazhang.arts@gmail.com>
2017-03-27 14:05:26 -0500

    changed GUIs

---
+++ Documents/SeniorDesignApplication/BreakingVADEchoSimulation-master/src/Calibration.java
@@ -0,0 +1,85 @@
+import com.google.firebase.database.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class Calibration {
+    private HashMap<String, ImagingRegion> calibratedRegions;
+    private DatabaseReference ref;
+    private String userID = "yWzgI0q39TRtvJUFhaapLFOfJAJ3";
+    private FirebaseDatabase database;
+    static Boolean wait;
+    private String[] positionCodes = {"PLA", "PSA", "A4C", "A2C", "SX4", "SPA"};
+
+    public Calibration() {
+        calibratedRegions = new HashMap<>();
+        database = ProbeDetection.getDatabase();
+    }
+
+    public void calibrate(String positionCode) {
+        String[] split = ProbeDetection.getInputLine().split(",");
+        if (split.length == 4) {
+            Long region = Long.parseLong(split[0]);
+            double xAng = Double.parseDouble(split[1]);
+            double yAng = Double.parseDouble(split[2]);
+            double zAng = Double.parseDouble(split[3]);
+
+            ImagingRegion imagingRegion = new ImagingRegion(region, xAng, yAng, zAng);
+            calibratedRegions.put(positionCode, imagingRegion);
+            System.out.println(calibratedRegions);
+        }
+    }
+
+    public HashMap<String, ImagingRegion> getCalibratedRegions() {
+        return calibratedRegions;
+    }
+
+    public void sendToFirebase(String saveName) {
+        ref = database.getReference(userID + "/SavedCalibrations/" + saveName);
+        ref.setValue(calibratedRegions);
+    }
+
+    public HashMap<String, HashMap<String, ImagingRegion>> getCalibrationsFromFirebase() {
+        ref = database.getReference(userID + "/SavedCalibrations/");
+
+        HashMap<String, HashMap<String, ImagingRegion>> savedCals = new HashMap<>();
+        wait = true;
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HashMap<String, ImagingRegion> childMap = new HashMap<>();
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    for (int i=0; i<6; i++) {
+                        try {
+                            childMap.put(positionCodes[i], new ImagingRegion(((HashMap<String, HashMap<String,Number>>) child.getValue()).get(positionCodes[i])));
+                            System.out.println(childMap);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    savedCals.put(child.getKey(), childMap);
+                    childMap = new HashMap<>();
+                }
+                wait = false;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // ...
+            }
+        });
+
+        while(wait) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ProbeDetection.setSavedCals(savedCals);
+
+        return savedCals;
+    }
+}
---
+++ Documents/SeniorDesignApplication/BreakingVADEchoSimulation-master/src/LoginListener.java
@@ -0,0 +1,56 @@
+import com.google.firebase.database.*;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class LoginListener implements ActionListener {
+    private LoginGUI loginGUI;
+    boolean retrievedFromFirebase = false;
+    public HashMap<String,String> users = new HashMap<String,String>();
+
+    public LoginListener() {
+        loginGUI = ProbeDetection.getLoginGUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (command.equals("Log in")) {
+            //TODO: get log in info from LoginGUI; validate with Firebase? OR just take username not password
+            String username = loginGUI.getUserNameInput().getText();
+            System.out.println("username: " + username);
+            FirebaseDatabase database = ProbeDetection.getDatabase();
+            DatabaseReference ref = database.getReference("Users Map");
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot child: dataSnapshot.getChildren()){
+                        HashMap<String,String> emailMap = (HashMap<String,String>) child.getValue();
+                        String key = child.getKey();
+                        String email = emailMap.get("email");
+                        users.put(key,email);
+
+                    }
+                    retrievedFromFirebase = true;
+                }
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+            while(!retrievedFromFirebase) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (users.containsValue(username)) {
+                JOptionPane.showMessageDialog(null, "SIGNED IN");
+                loginGUI.setStatus(true);
+            } else{
+                JOptionPane.showMessageDialog(null, "User is either invalid or isn't signed into web application");
+            }
+        }
+    }
+}
